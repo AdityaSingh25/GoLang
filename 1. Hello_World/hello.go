7f4f5cb227497db2342ca9c1bcde41cce3e518a7
Aditya Narayan Singh <richiaditya25@gmail.com>
2025-05-26 02:33:58 +0530

    short declaration, consts in compile time thing, Sprintf, printf, default values, %s, %v,%f, to evaluate values

--- 1. Hello_World/hello.go
+++ 1. Hello_World/hello.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 // func main(){
 // 	fmt.Print("Hello "+"World!!",rand.Intn(10))
@@ -49,10 +46,67 @@ import (
 // 	fmt.Printf("Type : %T Value is %v\n", z,z)
 // }
 
-func main(){
-	var x, y int = 3,4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
+// func main(){
+// 	var x, y int = 3,4
+// 	var f float64 = math.Sqrt(float64(x*x + y*y))
+// 	var z uint = uint(f)
 
-	fmt.Print(x,y,z)
+// 	fmt.Print(x,y,z)
+// }
+
+// func main(){
+// 	var username string= "Aditya"
+// 	var password string = "12345"
+// 	fmt.Println("Authorization: Basic", username+":"+password)
+// }
+
+// func main(){
+// 	var smsSendingLimit int
+// 	var costPerSms float64
+// 	var hasPermission bool
+// 	var username string
+
+// 	fmt.Printf("%v %f %v %q\n", smsSendingLimit,costPerSms,hasPermission,username,)
+// }
+
+// func main(){
+// 	// var empty string
+// 	// empty2 := ""
+// 	// they both are sae thing
+
+// 	congrats := "Happy birthday!!"
+// 	fmt.Println(congrats)
+// }
+
+// func main(){
+// 	penniesPerText := 2.0
+// 	fmt.Printf("The type of the variable is : %T\n",penniesPerText)
+// }
+
+// func main(){
+// 	avgOpenRate , displayMessage := 0.23, "message"
+
+// 	fmt.Printf("Types are %T,%T and values are %v,%v\n",avgOpenRate,displayMessage,avgOpenRate,displayMessage )
+
+// }
+
+// const did not allow the short declaration syntax
+
+// func main(){
+// 	const a = 5
+// 	const b = a+3
+
+// 	var x = 10
+// 	const y = x +1// error : as const should be known in complite time
+
+// 	fmt.Print(y)
+// }
+
+func main(){
+	name := "Aditya"
+	openRate := 20.99
+
+	msg := fmt.Sprintf("Hi %s your open rate is %.1f percent", name,openRate)
+
+	fmt.Println(msg)
 }
